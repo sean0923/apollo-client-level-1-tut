b466be301ecec7b58166973af30b6ff4d4560929
Sean Hong <iamseanhong@gmail.com>
2018-08-19 17:37:39 -0700

    Basic set up for apollo client

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react';
+
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
 import Header from './layout/Header';
 import Body from './layout/Body';
 
 import '../App.css';
 
+const client = new ApolloClient({
+  uri: 'https://api-uswest.graphcms.com/v1/cjl1iqlw3069d01f160ophfxl/master',
+});
+
 class Layout extends Component {
   render() {
     return (
-      <div className="App">
-        <Header />
-        <Body />
-      </div>
+      <ApolloProvider client={client}>
+        <div className="App">
+          <Header />
+          <Body />
+        </div>
+      </ApolloProvider>
     );
   }
 }
