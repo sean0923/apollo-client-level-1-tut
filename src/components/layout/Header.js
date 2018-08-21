d19f710ec152f150db5c338e904f6d4e7e574573
Sean Hong <iamseanhong@gmail.com>
2018-08-20 21:51:17 -0700

    Add style to header and link

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,17 +1,39 @@
 import React from 'react';
-import logo from '../../logo.svg';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
+import logo from '../../logo.svg';
 import '../../App.css';
+
+const FlexWrapper = styled.div`
+  margin-top: 1rem;
+
+  display: flex;
+  color: white;
+  justify-content: center;
+  align-items: flex-end;
+
+  & * {
+    margin: 0;
+    padding: 0;
+    line-height: 1;
+  }
+
+  & > *:not(:last-child) {
+    margin-right: 1rem;
+  }
+`;
 
 const Header = () => {
   return (
     <header className="App-header">
       <img src={logo} className="App-logo" alt="logo" />
-      <h1 className="App-title">Welcome to React</h1>
-      <Link to="/post/new">
-        <h2> New Post </h2>
-      </Link>
+      <FlexWrapper>
+        <h1>Welcome to React</h1>
+        <Link to="/post/new">
+          <h2> New Post </h2>
+        </Link>
+      </FlexWrapper>
     </header>
   );
 };
