74c01dcdef0bc414649078fd9327ece21a8889b1
Sean Hong <iamseanhong@gmail.com>
2018-08-21 20:26:35 -0700

    Style form

--- src/components/pages/NewPost.js
+++ src/components/pages/NewPost.js
@@ -1,26 +1,67 @@
 import React from 'react';
+import styled from 'styled-components';
+import gql from 'graphql-tag';
+import { Mutation } from 'react-apollo';
+
+// 
 import RenderPropsForForm from '../renderProps/RenderPropsForForm';
+
+// 
+const Wrapper = styled.div`
+  display: grid;
+  justify-content: center;
+`;
+
+const FormWrapper = styled.form`
+  margin: 2rem;
+  width: 300px;
+  padding: 2rem;
+  box-shadow: 0 0 1px rgba(0, 0, 0, 0.3);
+
+  display: grid;
+  grid-gap: 1rem;
+`;
+
+const GridWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 4rem 1fr;
+`;
+
+const ButtonWrapper = styled.button`justify-self: end;`;
+// 
+
 
 const NewPost = () => {
   return (
-    <div>
+    <Wrapper>
       <RenderPropsForForm state={{ title: '', body: '' }}>
         {({ state, handleOnChange, handleOnSubmit }) => {
           const { title, body } = state;
           return (
-            <form
-              action=""
+            <FormWrapper
               onSubmit={e => {
-                handleOnSubmit(e);
+                // handleOnSubmit(e);
+                e.preventDefault();
+                console.log('title: ', title);
+                console.log('body: ', body);
               }}
             >
-              <input name="title" value={title} type="text" onChange={handleOnChange} />
-              <textarea name="body" value={body} type="text" onChange={handleOnChange} />
-            </form>
+              <GridWrapper>
+                <label>title:</label>
+                <input name="title" value={title} type="text" onChange={handleOnChange} />
+              </GridWrapper>
+
+              <GridWrapper>
+                <label>body:</label>
+                <textarea name="body" value={body} type="text" onChange={handleOnChange} />
+              </GridWrapper>
+
+              <ButtonWrapper>Submit</ButtonWrapper>
+            </FormWrapper>
           );
         }}
       </RenderPropsForForm>
-    </div>
+    </Wrapper>
   );
 };
 
